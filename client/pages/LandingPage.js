9d9937cd1ba2263e0d252a86a02e1de1dec93ab2
Devon Richards <friends.devon@gmail.com>
2021-08-05 21:53:56 -0500

    Add logos.

--- client/pages/LandingPage.js
+++ client/pages/LandingPage.js
@@ -37,10 +37,10 @@ export const LandingPage = ({ numusers, numcubes, numdrafts }) => {
   return (
     <div className="flex-container flex-vertical viewport">
       <Row className="m-0 p-0 flex-grow">
-        <Col xs="12" sm="6" className="m-0 bg-green landing-half landing-logo-container">
+        <Col xs="12" sm="6" className="m-0 bg-dark landing-half landing-logo-container">
           <img src="/content/LandingLogo.png" alt={siteName} className="landing-logo" />
         </Col>
-        <Col xs="12" sm="6" className="m-0 bg-dark landing-half flex-container flex-vertical">
+        <Col xs="12" sm="6" className="m-0 bg-accent landing-half flex-container flex-vertical">
           <div className="mt-3 flex-container">
             <CardSearchBar />
           </div>
