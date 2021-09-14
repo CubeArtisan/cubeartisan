facc06b37d7304bfe9c566f36361db546e66c59a
Devon Richards <friends.devon@gmail.com>
2021-09-14 17:55:43 -0500

    Accept "TRUE" for maybeboard to support excel edit.
    Ported over from dekkerglen/CubeCobra#2140.

--- server/serverjs/cubefn.js
+++ server/serverjs/cubefn.js
@@ -277,7 +277,7 @@ export const CSVtoCards = (csvString, carddb) => {
         const nonPromo = potentialIds.find(carddb.reasonableId);
         const first = potentialIds[0];
         card.cardID = matchingSetAndNumber || matchingSet || nonPromo || first;
-        if (maybeboard === 'true') {
+        if (maybeboard.toLowerCase() === 'true') {
           newMaybe.push(card);
         } else {
           newCards.push(card);
