cf1ae131a9ae8852f3aeec72f14ae6aef002376d
Devon Richards <friends.devon@gmail.com>
2021-11-10 21:43:45 -0600

    chore: Fix lint for draftlog-import.

--- server/routes/integrations.js
+++ server/routes/integrations.js
@@ -19,11 +19,11 @@ import { wrapAsyncApi } from '@cubeartisan/server/routes/middleware.js';
 
 const API_KEY = process.env.CUBEARTISAN_API_KEY;
 const DEFAULT_BASICS = [
-  "56719f6a-1a6c-4c0a-8d21-18f7d7350b68",
-  "b2c6aa39-2d2a-459c-a555-fb48ba993373",
-  "bc71ebf6-2056-41f7-be35-b2e5c34afa99",
-  "b34bb2dc-c1af-4d77-b0b3-a0fb342a5fc6",
-  "a3fb7228-e76b-4e96-a40e-20b5fed75685",
+  '56719f6a-1a6c-4c0a-8d21-18f7d7350b68',
+  'b2c6aa39-2d2a-459c-a555-fb48ba993373',
+  'bc71ebf6-2056-41f7-be35-b2e5c34afa99',
+  'b34bb2dc-c1af-4d77-b0b3-a0fb342a5fc6',
+  'a3fb7228-e76b-4e96-a40e-20b5fed75685',
 ];
 
 const importDraftLogHandler = async (req, res) => {
