d321b39454aeadf0e1805cdca4e6e2c43318fff4
Devon Richards <friends.devon@gmail.com>
2021-12-06 23:56:13 -0600

    Fix spacing of DeckPage.

--- client/components/DeckCard.js
+++ client/components/DeckCard.js
@@ -16,9 +16,10 @@
  *
  * Modified from the original version in CubeCobra. See LICENSE.CubeCobra for more information.
  */
-import React, { lazy } from 'react';
+import React, { lazy, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { CircularProgress, Grid, Paper, Stack, Typography } from '@mui/material';
+import DisplayContext from '@cubeartisan/client/components/contexts/DisplayContext.js';
 
 import CardPropType from '@cubeartisan/client/proptypes/CardPropType.js';
 import DraftSeatPropType from '@cubeartisan/client/proptypes/DraftSeatPropType.js';
@@ -26,36 +27,35 @@ import DeckPropType from '@cubeartisan/client/proptypes/DeckPropType.js';
 import CommentsSection from '@cubeartisan/client/components/CommentsSection.js';
 import FoilCardImage from '@cubeartisan/client/components/FoilCardImage.js';
 import Markdown from '@cubeartisan/client/components/markdown/Markdown.js';
-import { makeSubtitle } from '@cubeartisan/client/utils/Card.js';
 import Suspense from '@cubeartisan/client/components/wrappers/Suspense.js';
 import CardHeader from '@cubeartisan/client/components/CardHeader.js';
 
 const DecksPickBreakdown = lazy(async () => import('@cubeartisan/client/components/DecksPickBreakdown.js'));
 const DraftbotBreakdown = lazy(async () => import('@cubeartisan/client/components/DraftbotBreakdown.js'));
 
-const DeckStacksStatic = ({ piles, cards }) => (
-  <Grid container>
+const DeckStacksStatic = ({ piles, cards, cardsInRow }) => (
+  <Grid container columns={cardsInRow}>
     {piles.map((row, index) =>
       row
         .map((column, index2) => (
-          <Grid key={/* eslint-disable-line react/no-array-index-key */ `${index}-${index2}`} item xs={3}>
+          <Grid key={/* eslint-disable-line react/no-array-index-key */ `${index}-${index2}`} item xs={1}>
             {column.length > 0 && (
               <Typography align="center" variant="body1" fontWeight="bold">
                 {column.length}
               </Typography>
             )}
-            <Stack>
+            <div className="stack">
               {column.map((cardIndex, index3) => {
                 const card = cards[cardIndex];
                 return (
-                  <div className="stacked" key={/* eslint-disable-line react/no-array-index-key */ index3}>
+                  <div key={/* eslint-disable-line react/no-array-index-key */ index3} className="stacked">
                     <a href={card.cardID ? `/card/${card.cardID}` : null}>
                       <FoilCardImage card={card} tags={[]} autocard />
                     </a>
                   </div>
                 );
               })}
-            </Stack>
+            </div>
           </Grid>
         ))
         .flat(2),
@@ -66,9 +66,11 @@ const DeckStacksStatic = ({ piles, cards }) => (
 DeckStacksStatic.propTypes = {
   cards: PropTypes.arrayOf(PropTypes.shape({ cardID: PropTypes.string })).isRequired,
   piles: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number.isRequired))).isRequired,
+  cardsInRow: PropTypes.number.isRequired,
 };
 
 const DeckCard = ({ seat, deck, seatIndex, draft, view }) => {
+  const { cardsInRow } = useContext(DisplayContext);
   const [username] = deck.seats[seatIndex].username.split(':');
   const draftSeatIndex = draft.seats.findIndex(({ name }) => name === username);
   console.log('draftSeatIndex', draftSeatIndex);
@@ -125,23 +127,13 @@ const DeckCard = ({ seat, deck, seatIndex, draft, view }) => {
           ))}
         {view === 'deck' && (
           <>
-            <DeckStacksStatic
-              piles={stackedDeck}
-              cards={deck.cards}
-              title="Deck"
-              subtitle={makeSubtitle(
-                seat.deck
-                  .flat()
-                  .flat()
-                  .map((cardIndex) => deck.cards[cardIndex]),
-              )}
-            />
+            <DeckStacksStatic piles={stackedDeck} cards={deck.cards} cardsInRow={cardsInRow} />
             {stackedSideboard && stackedSideboard.length > 0 && (
               <>
                 <CardHeader className="border-bottom">
                   <h4>Sideboard</h4>
                 </CardHeader>
-                <DeckStacksStatic piles={stackedSideboard} cards={deck.cards} title="Sideboard" />
+                <DeckStacksStatic cardsInRow={cardsInRow} piles={stackedSideboard} cards={deck.cards} />
               </>
             )}
           </>
