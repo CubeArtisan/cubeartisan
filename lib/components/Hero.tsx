2917fa2d2c8dd5172ab4010fae90768ed6e111bf
jesseb34r <aubin.1+github@gmail.com>
2022-11-02 23:25:18 -0700

    change hero size

--- lib/components/Hero.tsx
+++ lib/components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero({
   children?: React.ReactNode;
 }) {
   return (
-    <section className="flex min-h-[10rem] w-full flex-col items-center justify-center bg-gradient-to-r from-transparent via-blue-300 to-transparent">
+    <section className="flex min-h-[15rem] w-full flex-col items-center justify-center bg-gradient-to-r from-transparent via-blue-300 to-transparent">
       <h1 className="mb-1 text-[2.5rem]">{mainText}</h1>
       <p className="text-md text-gray-600">{subText}</p>
       {children}
