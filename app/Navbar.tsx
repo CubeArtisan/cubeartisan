da61a3a7904f2ac410c87165c53dc254afea6d6e
jesseb34r <aubin.1+github@gmail.com>
2022-11-03 00:23:18 -0700

    remove unnecessary custom global css class in favor of arbitrary inline tailwind

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,7 @@ import BellIcon from "@heroicons/react/24/solid/BellIcon";
 
 export default function Navbar() {
   return (
-    <header className="clamp-app mt-4 mb-8 flex px-2 pt-3 leading-7">
+    <header className="mx-auto mt-4 mb-8 flex px-2 pt-3 leading-7 [width:clamp(100px,_80%,_90%)]">
       <Link className="mr-8 text-xl font-semibold" href="/">
         CubeArtisan
       </Link>
