d10e1096e8ebcb5addef9c98f1be490a84829090
jesseb34r <aubin.1+github@gmail.com>
2022-11-04 04:32:09 -0700

    fix(config): add webpack config for tailwind, vscode npm manager setting, and next scripts in @cubeartisan/next/

--- next/next.config.js
+++ next/next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')([
-  '@mui/material',
-  '@mui/system',
-  '@mui/icons-material', // If @mui/icons-material is being used
-]);
+// const withTM = require('next-transpile-modules')([
+//   '@mui/material',
+//   '@mui/system',
+//   '@mui/icons-material', // If @mui/icons-material is being used
+// ]);
 
 module.exports = {
-  ...withTM({
-    webpack: (config) => {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@mui/styled-engine': '@mui/styled-engine-sc',
-      };
-      return config;
-    },
-  }),
+  // ...withTM({
+  //   webpack: (config) => {
+  //     config.resolve.alias = {
+  //       ...config.resolve.alias,
+  //       '@mui/styled-engine': '@mui/styled-engine-sc',
+  //     };
+  //     return config;
+  //   },
+  // }),
   experimental: {
     appDir: true,
   },
---
+++ next/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
