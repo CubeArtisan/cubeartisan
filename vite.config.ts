947369aa5bc9488ae62106926639eb9c4dddc67d
jesseb34r <aubin.1+github@gmail.com>
2022-11-21 18:38:40 -0800

    feat(deps): add vanilla-extract to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path';
 
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       '@cubeartisan/cubeartisan': path.resolve(__dirname, './src/'),
