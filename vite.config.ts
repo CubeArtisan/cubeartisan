0d4ff44bfd19cd88b9d1f359bda1b9358710364f
jesseb34r <aubin.1+github@gmail.com>
2022-11-21 18:36:45 -0800

    fix(config): commented out test config
    vite was throwing a type error and I couldn't find docs for test config

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,13 @@ export default defineConfig({
       '@cubeartisan/cubeartisan': path.resolve(__dirname, './src/'),
     },
   },
-  test: {
-    deps: {
-      registerNodeLoader: true,
-    },
-    globals: true,
-    isolate: true,
-    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect'],
-    transformMode: { web: [/\.[jt]sx$/] },
-  },
+  // test: {
+  //   deps: {
+  //     registerNodeLoader: true,
+  //   },
+  //   globals: true,
+  //   isolate: true,
+  //   setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect'],
+  //   transformMode: { web: [/\.[jt]sx$/] },
+  // },
 });
