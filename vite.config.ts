cf48f701ab4162d031101498b938f4e4cb7a77bd
jesseb34r <aubin.1+github@gmail.com>
2022-11-21 21:28:40 -0800

    fix(config): use correct config import

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import solid from 'solid-start/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [solid(), vanillaExtractPlugin()],
@@ -11,13 +11,13 @@ export default defineConfig({
       '@cubeartisan/cubeartisan': path.resolve(__dirname, './src/'),
     },
   },
-  // test: {
-  //   deps: {
-  //     registerNodeLoader: true,
-  //   },
-  //   globals: true,
-  //   isolate: true,
-  //   setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect'],
-  //   transformMode: { web: [/\.[jt]sx$/] },
-  // },
+  test: {
+    deps: {
+      registerNodeLoader: true,
+    },
+    globals: true,
+    isolate: true,
+    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect'],
+    transformMode: { web: [/\.[jt]sx$/] },
+  },
 });
