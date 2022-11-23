1f97238b5a44b3e09b083acc9ba0e85e1ef91512
jesseb34r <aubin.1+github@gmail.com>
2022-11-23 07:23:22 -0800

    feat(style): add typography and breakpoint styles

---
+++ src/theme/screens.css.ts
@@ -0,0 +1,18 @@
+/**
+ * min: portrait phone
+ * sm: landscape phone
+ * md: tablet
+ * lg: laptop
+ * xl: desktop
+ * 2xl: ultrawide
+ */
+const breakpointsRem = {
+  min: 20,
+  sm: 36,
+  md: 48,
+  lg: 64,
+  xl: 80,
+  '2xl': 96,
+};
+
+export default breakpointsRem;
---
+++ src/theme/typography.css.ts
@@ -0,0 +1,41 @@
+import screens from '@cubeartisan/cubeartisan/theme/screens.css';
+
+// font size settings in rem
+const typographyConfig = {
+  baseMin: 1.125,
+  baseMax: 1.25,
+  minMultiScale: 1.125,
+  maxMultiScale: 1.2,
+};
+
+/**
+ * used to calculate variable clamp properties for fluid typography tailwind classes
+ *
+ * clamp([min]rem, [v]vw + [r]rem , [max]rem)
+ *
+ * y = (v/100) * x + r
+ *
+ * x = current viewport width
+ * y = resulting fluid font size
+ * v = viewport modifier to calculate font size
+ * r = relative size from default font size
+ *
+ * source: https://www.smashingmagazine.com/2022/01/modern-fluid-typography-css-clamp/
+ */
+const fontSizeScale = (multi: number) => {
+  /**
+   * note that with this implementation, negative multipliers result in static clamp values
+   * as min will be larger than max. This is intended and ok
+   */
+  const minFont = typographyConfig.baseMin * typographyConfig.minMultiScale ** multi;
+  const maxFont = typographyConfig.baseMax * typographyConfig.maxMultiScale ** multi;
+  const minScreen = screens.min;
+  const maxScreen = screens['2xl'];
+
+  const v = (100 * (maxFont - minFont)) / (maxScreen - minScreen);
+  const r = (maxScreen * minFont - minScreen * maxFont) / (maxScreen - minScreen);
+
+  return `clamp(${minFont}rem, ${v}vw + ${r}rem , ${maxFont}rem)`;
+};
+
+export default fontSizeScale;
