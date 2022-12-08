447c15ced066de2d94730c8e153e429037cbdee5
jesseb34r <aubin.1+github@gmail.com>
2022-12-08 00:59:47 -0800

    chore(style): use sprinkles in nav.css.ts

--- src/components/Nav.css.ts
+++ src/components/Nav.css.ts
@@ -1,26 +1,38 @@
 import { style } from '@vanilla-extract/css';
 
-import { colors, vars } from '@cubeartisan/cubeartisan/styles';
+import { colors, sprinkles, vars } from '@cubeartisan/cubeartisan/styles';
 
-export const nav = style({
-  height: vars.space[12],
-  background: colors.neutral[2],
-  color: colors.neutral[12],
-  width: '100%',
-});
+export const nav = style([
+  sprinkles({
+    height: 12,
+    width: 'full',
+  }),
+  {
+    background: colors.neutral[2],
+    color: colors.neutral[12],
+  },
+]);
 
-export const navContainer = style({
-  display: 'flex',
-  justifyContent: 'space-between',
-  width: `clamp(${vars.sizes.sm}, 80%, ${vars.sizes['5xl']})`,
-  height: '100%',
-  marginInline: 'auto',
-  fontSize: vars.fontSize.base,
-});
+export const navContainer = style([
+  sprinkles({
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: 'full',
+    fontSize: 'base',
+  }),
+  {
+    width: `clamp(${vars.sizes.sm}, 80%, ${vars.sizes['5xl']})`,
+    marginInline: 'auto',
+  },
+]);
 
-export const navList = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  gap: vars.space[3],
-});
+export const navList = style([
+  sprinkles({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }),
+  {
+    gap: vars.space[3],
+  },
+]);
