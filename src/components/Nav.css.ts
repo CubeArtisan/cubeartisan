601e4cb69effe5b333cf987e1632d18a8eab4d82
jesseb34r <aubin.1+github@gmail.com>
2022-12-13 14:52:19 -0800

    chore(style): use atoms as much as possible

--- src/components/Nav.css.ts
+++ src/components/Nav.css.ts
@@ -1,38 +1,24 @@
-import { style } from '@vanilla-extract/css';
+import { atoms } from '@cubeartisan/cubeartisan/styles';
 
-import { atoms, vars } from '@cubeartisan/cubeartisan/styles';
+export const nav = atoms({
+  height: 12,
+  width: 'full',
+  backgroundColor: 'neutralSubtleSecondary',
+  color: 'neutralContrast',
+});
 
-export const nav = style([
-  atoms({
-    height: 12,
-    width: 'full',
-  }),
-  {
-    background: vars.backgroundColor.neutral.neutralSubtleSecondary,
-    color: vars.textColor.neutral.neutralContrast,
-  },
-]);
+export const navContainer = atoms({
+  display: 'flex',
+  justifyContent: 'spaceBetween',
+  height: 'full',
+  fontSize: 'base',
+  marginInline: 'auto',
+  width: 'content-80',
+});
 
-export const navContainer = style([
-  atoms({
-    display: 'flex',
-    justifyContent: 'spaceBetween',
-    height: 'full',
-    fontSize: 'base',
-  }),
-  {
-    width: `clamp(${vars.size.sm}, 80%, ${vars.size['5xl']})`,
-    marginInline: 'auto',
-  },
-]);
-
-export const navList = style([
-  atoms({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'spaceBetween',
-  }),
-  {
-    gap: vars.space[3],
-  },
-]);
+export const navList = atoms({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'spaceBetween',
+  gap: 3,
+});
