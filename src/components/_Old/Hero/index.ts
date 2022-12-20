1eba9f6a13e6ce1103f13b432288ebb598e972b8
jesseb34r <aubin.1+github@gmail.com>
2022-12-20 09:36:24 -0800

    chore(components): clean up depracated files

--- src/components/_Old/Hero/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  Hero,
-  HeroContentBlock,
-  //
-  Root,
-  ContentBlock,
-} from '@cubeartisan/cubeartisan/components/_Old/Hero/Hero';
-export type { HeroProps, HeroContentBlockProps } from '@cubeartisan/cubeartisan/components/_Old/Hero/Hero';
