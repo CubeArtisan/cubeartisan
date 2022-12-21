6a8a5c7a1b2ad01d076c94e248a12e2298d9ca53
jesseb34r <aubin.1+github@gmail.com>
2022-12-20 22:33:19 -0800

    feat(app): add test page for prototyping components

---
+++ src/routes/(app)/test.tsx
@@ -0,0 +1,9 @@
+// for testing components
+import artisan from '@cubeartisan/cubeartisan/components/factory';
+
+const Test = () => {
+  const testdata = null;
+  return <artisan.h1>Some test text</artisan.h1>;
+};
+
+export default Test;
