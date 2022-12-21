fb0d6fa4f86c13ac7c03dd094ea6583e5d5f2075
jesseb34r <aubin.1+github@gmail.com>
2022-12-20 23:17:38 -0800

    feat(components): add Button component

---
+++ src/components/generic/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { splitProps } from 'solid-js';
+
+import artisan from '@cubeartisan/cubeartisan/components/factory';
+import { buttonRecipe, ButtonVariants } from '@cubeartisan/cubeartisan/components/generic/Button/Button.css';
+import type { ArtisanComponent } from '@cubeartisan/cubeartisan/components/types';
+
+export const Button: ArtisanComponent<'button', ButtonVariants> = (props) => {
+  const [local, others] = splitProps(props, ['recipe']);
+
+  return <artisan.button class={buttonRecipe(local.recipe)} {...others} />;
+};
