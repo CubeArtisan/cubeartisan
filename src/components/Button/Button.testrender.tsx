112405e37b60d102cf7033e372a79e49c7dc5339
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 13:54:28 -0800

    chore(components): remove redundant Inline (replaced with HStack)

--- src/components/Button/Button.testrender.tsx
+++ src/components/Button/Button.testrender.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@cubeartisan/cubeartisan/components/Button/Button';
-import { Inline } from '@cubeartisan/cubeartisan/components/Inline';
-import { Stack } from '@cubeartisan/cubeartisan/components/Stack';
+import { HStack, Stack } from '@cubeartisan/cubeartisan/components/Stack';
 
 export const TestButton = () => {
   const testSizes = {
@@ -16,30 +15,30 @@ export const TestButton = () => {
 
   return (
     <Stack as="main" atoms={{ minHeight: 'screenH', width: 'content-60', marginInline: 'auto' }}>
-      <Inline>
+      <HStack>
         <Button recipe={{ size: testSize, color: 'neutral' }}>neutral</Button>
         <Button recipe={{ size: testSize, color: 'primary' }}>primary</Button>
         <Button recipe={{ size: testSize, color: 'info' }}>info</Button>
         <Button recipe={{ size: testSize, color: 'success' }}>success</Button>
         <Button recipe={{ size: testSize, color: 'warning' }}>warning</Button>
         <Button recipe={{ size: testSize, color: 'danger' }}>danger</Button>
-      </Inline>
-      <Inline>
+      </HStack>
+      <HStack>
         <Button recipe={{ size: testSize, color: 'neutralSolid' }}>neutral</Button>
         <Button recipe={{ size: testSize, color: 'primarySolid' }}>primary</Button>
         <Button recipe={{ size: testSize, color: 'infoSolid' }}>info</Button>
         <Button recipe={{ size: testSize, color: 'successSolid' }}>success</Button>
         <Button recipe={{ size: testSize, color: 'warningSolid' }}>warning</Button>
         <Button recipe={{ size: testSize, color: 'dangerSolid' }}>danger</Button>
-      </Inline>
-      <Inline>
+      </HStack>
+      <HStack>
         <Button recipe={{ size: testSizes.xs, color: 'neutralSolid' }}>neutral</Button>
         <Button recipe={{ size: testSizes.sm, color: 'primarySolid' }}>primary</Button>
         <Button recipe={{ size: testSizes.md, color: 'infoSolid' }}>info</Button>
         <Button recipe={{ size: testSizes.lg, color: 'successSolid' }}>success</Button>
         <Button recipe={{ size: testSizes.xl, color: 'warningSolid' }}>warning</Button>
         <Button recipe={{ size: testSizes['2xl'], color: 'dangerSolid' }}>danger</Button>
-      </Inline>
+      </HStack>
     </Stack>
   );
 };
--- src/components/Inline/Inline.tsx
+++
@@ -1,4 +0,0 @@
-import artisan from '@cubeartisan/cubeartisan/components/factory';
-import { inlineRecipe } from '@cubeartisan/cubeartisan/components/Inline/Inline.css';
-
-export const Inline = artisan('div', inlineRecipe);
