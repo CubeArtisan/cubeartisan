e045ef627ab3866d6977029b431594c8c6311d03
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 12:31:28 -0800

    feat(Stack): add HStack and VStack versions of Stack

--- src/components/Stack/VStack.tsx
+++ src/components/Stack/VStack.tsx
@@ -1,12 +1,13 @@
+import { merge } from 'lodash';
 import { mergeProps, splitProps } from 'solid-js';
 
 import artisan from '@cubeartisan/cubeartisan/components/factory';
 import { stackRecipe, StackVariants } from '@cubeartisan/cubeartisan/components/Stack/Stack.css';
 import type { ArtisanComponent } from '@cubeartisan/cubeartisan/components/types';
 
-export const Stack: ArtisanComponent<'div', StackVariants> = (props) => {
+export const VStack: ArtisanComponent<'div', Omit<StackVariants, 'direction'>> = (props) => {
   const propsWithDefault = mergeProps({ space: 'md', align: 'center' }, props);
   const [local, others] = splitProps(propsWithDefault, ['recipe']);
 
-  return <artisan.div class={stackRecipe(local.recipe)} {...others} />;
+  return <artisan.div class={stackRecipe(merge({ direction: 'column' }, local.recipe))} {...others} />;
 };
