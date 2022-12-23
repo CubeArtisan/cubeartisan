3345dfd81dd87352fc2613de135f9f008c04c6a1
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 12:31:28 -0800

    feat(Stack): add HStack and VStack versions of Stack

--- src/components/Stack/Stack.css.ts
+++ src/components/Stack/Stack.css.ts
@@ -2,25 +2,65 @@ import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 
 import { atoms } from '@cubeartisan/cubeartisan/styles';
 
+const spacing = {
+  xs: 1,
+  sm: 2,
+  md: 3,
+  lg: 5,
+  xl: 8,
+};
+
 export const stackRecipe = recipe({
   base: atoms({
     display: 'flex',
     flexDirection: 'column',
+    gap: spacing.md,
+    flexWrap: 'nowrap',
+    alignItems: 'center',
     justifyContent: 'center',
   }),
   variants: {
-    space: {
-      xs: atoms({ gap: 1 }),
-      sm: atoms({ gap: 2 }),
-      md: atoms({ gap: 3 }),
-      lg: atoms({ gap: 5 }),
-      'space-between': atoms({ justifyContent: 'spaceBetween' }),
-      'space-around': atoms({ justifyContent: 'spaceAround' }),
+    direction: {
+      row: atoms({ flexDirection: 'row' }),
+      column: atoms({ flexDirection: 'column' }),
+    },
+    spacing: {
+      xs: atoms({ gap: spacing.xs }),
+      sm: atoms({ gap: spacing.sm }),
+      md: atoms({ gap: spacing.md }),
+      lg: atoms({ gap: spacing.lg }),
+      xl: atoms({ gap: spacing.xl }),
+    },
+    spacingX: {
+      xs: atoms({ rowGap: spacing.xs }),
+      sm: atoms({ rowGap: spacing.sm }),
+      md: atoms({ rowGap: spacing.md }),
+      lg: atoms({ rowGap: spacing.lg }),
+      xl: atoms({ rowGap: spacing.xl }),
+    },
+    spacingY: {
+      xs: atoms({ columnGap: spacing.xs }),
+      sm: atoms({ columnGap: spacing.sm }),
+      md: atoms({ columnGap: spacing.md }),
+      lg: atoms({ columnGap: spacing.lg }),
+      xl: atoms({ columnGap: spacing.xl }),
+    },
+    wrap: {
+      nowrap: atoms({ flexWrap: 'nowrap' }),
+      wrap: atoms({ flexWrap: 'wrap' }),
     },
     align: {
-      left: atoms({ alignItems: 'flexStart' }),
-      right: atoms({ alignItems: 'flexEnd' }),
+      start: atoms({ alignItems: 'flexStart' }),
+      end: atoms({ alignItems: 'flexEnd' }),
       center: atoms({ alignItems: 'center' }),
+    },
+    justify: {
+      start: atoms({ justifyContent: 'flexStart' }),
+      end: atoms({ justifyContent: 'flexEnd' }),
+      center: atoms({ justifyContent: 'center' }),
+      'space-around': atoms({ justifyContent: 'spaceAround' }),
+      'space-between': atoms({ justifyContent: 'spaceBetween' }),
+      'space-evenly': atoms({ justifyContent: 'spaceEvenly' }),
     },
   },
 });
