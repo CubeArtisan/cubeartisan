955108443bbe30e7c4ea703e3f7efead599bdaba
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 12:31:28 -0800

    feat(Stack): add HStack and VStack versions of Stack

--- src/components/Stack/index.ts
+++ src/components/Stack/index.ts
@@ -1,1 +1,3 @@
 export * from '@cubeartisan/cubeartisan/components/Stack/Stack';
+export * from '@cubeartisan/cubeartisan/components/Stack/HStack';
+export * from '@cubeartisan/cubeartisan/components/Stack/VStack';
