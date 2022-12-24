0637f3f6c7e1f7d88615d25101985d5afe63bc9b
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 23:10:07 -0800

    chore(button): use new syntax for defining artisan components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,5 @@
-import { splitProps } from 'solid-js';
-
 import { buttonRecipe, ButtonVariants } from '@cubeartisan/cubeartisan/components/Button/Button.css';
 import artisan from '@cubeartisan/cubeartisan/components/factory';
 import type { ArtisanComponent } from '@cubeartisan/cubeartisan/components/types';
 
-export const Button: ArtisanComponent<'button', ButtonVariants> = (props) => {
-  const [local, others] = splitProps(props, ['recipe']);
-
-  return <artisan.button class={buttonRecipe(local.recipe)} {...others} />;
-};
+export const Button: ArtisanComponent<'button', ButtonVariants> = artisan('button', buttonRecipe);
