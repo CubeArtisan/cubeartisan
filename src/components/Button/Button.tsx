771afadd7e442fbf00841ee1e56ad385e96ef570
jesseb34r <aubin.1+github@gmail.com>
2022-12-23 23:10:07 -0800

    chore(button): use new syntax for defining artisan components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button/Button.css';
+import { buttonRecipe, ButtonVariants } from '@cubeartisan/cubeartisan/components/Button/Button.css';
 import artisan from '@cubeartisan/cubeartisan/components/factory';
 
-export const Button = artisan('button', buttonRecipe);
+export const Button: ArtisanComponent<'button', ButtonVariants> = artisan('button', buttonRecipe);
