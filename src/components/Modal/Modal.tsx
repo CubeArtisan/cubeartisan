364da26ba5a88f62541f0443cb8b4cab8c19a097
jesseb34r <aubin.1+github@gmail.com>
2022-12-28 23:28:55 -0800

    chore(modal): reset to basic implementation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,147 +1,45 @@
-// # Planning
-// ## Elements:
-// - Root
-// - Context Provider
-// - Portal
-// - Overlay
-// - Title (for visuals and accessibility) | Header
-// - Description (mostly for accessibility I think?)
-// - Content | Body
-//
-// ## Features
-// - Focus lock
-// - Scroll lock
-// - click outside closes without submitting form
-// - keyboard navigation that follows aria standards
-import { createContext, createUniqueId, Show, useContext } from 'solid-js';
-import { createStore } from 'solid-js/store';
+import { createSignal, Show } from 'solid-js';
 import { Portal } from 'solid-js/web';
 
-import type { ArtisanParentComponent } from '@cubeartisan/cubeartisan/components/types';
+import { Button } from '@cubeartisan/cubeartisan/components/Button';
+import artisan from '@cubeartisan/cubeartisan/components/factory';
+import * as Form from '@cubeartisan/cubeartisan/components/Form';
+import type { ArtisanComponent } from '@cubeartisan/cubeartisan/components/types';
 
-interface ModalProps {
-  isOpen: boolean;
-  id?: string;
-  closeOnOverlayClick?: boolean;
-  closeOnEsc?: boolean;
-  onClose: () => void;
-  onOverlayClick?: () => void;
-  onEsc?: () => void;
-  // initialFocus?: string - TODO
-  blockScrollOnMount?: boolean;
-  // transition: TODO
-  centered?: boolean;
-}
-
-interface ModalState extends Required<Pick<ModalProps, 'isOpen', 'closeOnOverlayClick', 'closeOnEsc'>> {
-  modalID: string;
-  titleID: string;
-  descriptionID: string;
-  contentID: string;
-}
-
-interface ModalContextValue {
-  state: ModalState;
-  onClose: () => void;
-  onOverlayClick: (event: MouseEvent) => void;
-  onMouseDown: (event: MouseEvent) => void;
-  onKeyDown: (event: KeyboardEvent) => void;
-}
-
-const ModalContext = createContext<ModalContextValue>();
-
-const useModalContext = () => {
-  const context = useContext(ModalContext);
-
-  if (!context) {
-    throw new Error('[Artisan]: useModalContext must be used within a `<Modal.Root />` component');
-  }
-
-  return context;
-};
-
-const Modal: ArtisanParentComponent<'div', Record<string, never>, ModalProps> = (props) => {
-  const defaultModalID = `artisan-modal-${createUniqueId()}`;
-
-  const [state, setState] = createStore<ModalState>({
-    get isOpen() {
-      return props.isOpen;
-    },
-    get modalID() {
-      return props.id ?? defaultModalID;
-    },
-    get titleID() {
-      return `${this.modalID}--title`;
-    },
-    get descriptionID() {
-      return `${this.modalID}--description`;
-    },
-    get contentID() {
-      return `${this.modalID}--content`;
-    },
-    get closeOnOverlayClick() {
-      return props.closeOnOverlayClick ?? true;
-    },
-    get closeOnEsc() {
-      return props.closeOnEsc ?? true;
-    },
-  });
-
-  const onClose = () => props.onClose();
-
-  let mouseDownTarget: EventTarget | null = null;
-
-  const onMouseDown = (event: MouseEvent) => {
-    mouseDownTarget = event.target;
-  };
-
-  const onKeyDown = (event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      event.stopPropagation();
-
-      if (state.closeOnEsc) {
-        onClose();
-      }
-
-      props.onEsc?.();
-    }
-  };
-
-  const onOverlayClick = (event: MouseEvent) => {
-    event.stopPropagation();
-    /**
-     * Prevent the modal from closing when user
-     * start dragging from the content, and release drag outside the content.
-     *
-     * Because it is technically not a considered "click outside".
-     */
-    if (mouseDownTarget !== event.target) {
-      return;
-    }
-
-    if (state.closeOnOverlayClick) {
-      onClose();
-    }
-
-    props.onOverlayClick?.();
-  };
-
-  const context: ModalContextValue = {
-    state,
-    onClose,
-    onMouseDown,
-    onKeyDown,
-    onOverlayClick,
-  };
+export const Modal: ArtisanComponent<'div'> = (props) => {
+  const [isOpen, setIsOpen] = createSignal();
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
   return (
-    <Show when={props.isOpen()}>
-      <ModalContext.Provider value={context}>
-        <Portal>{props.children}</Portal>
-      </ModalContext.Provider>
-    </Show>
+    <>
+      <Button onClick={open}>Open Modal</Button>
+      <Show when={isOpen()}>
+        <Portal>
+          <artisan.section
+            atoms={{
+              position: 'absolute',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              padding: 4,
+              backgroundColor: 'neutralComponent',
+            }}
+            style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}
+          >
+            <artisan.header>
+              <artisan.h1>Modal Title</artisan.h1>
+              <artisan.p>A test modal that displays placeholders instead of content</artisan.p>
+            </artisan.header>
+            <artisan.div>
+              <Button onClick={close} recipe={{ size: 'sm' }}>
+                Close
+              </Button>
+            </artisan.div>
+          </artisan.section>
+        </Portal>
+      </Show>
+    </>
   );
 };
-
-export { Modal as Root, useModalContext };
-export type { ModalProps, ModalContextValue };
