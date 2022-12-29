07fbd5a426b3be3b03d320be76fc330bf3cb16e2
jesseb34r <aubin.1+github@gmail.com>
2022-12-28 23:28:55 -0800

    chore(modal): reset to basic implementation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,13 @@
 import { createSignal, Show } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 import { Portal } from 'solid-js/web';
 
 import { Button } from '@cubeartisan/cubeartisan/components/Button';
 import artisan from '@cubeartisan/cubeartisan/components/factory';
+import * as Form from '@cubeartisan/cubeartisan/components/Form';
 import type { ArtisanComponent } from '@cubeartisan/cubeartisan/components/types';
 
-export const Modal: ArtisanComponent<'div'> = () => {
+export const Modal: ArtisanComponent<'div'> = (props) => {
   const [isOpen, setIsOpen] = createSignal();
   const open = () => setIsOpen(true);
   const close = () => setIsOpen(false);
@@ -16,6 +18,35 @@ export const Modal: ArtisanComponent<'div'> = () => {
         <Portal>{props.children}</Portal>
       </ModalContext.Provider>
     </Show>
+    <>
+      <Button onClick={open}>Open Modal</Button>
+      <Show when={isOpen()}>
+        <Portal>
+          <artisan.section
+            atoms={{
+              position: 'absolute',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              padding: 4,
+              backgroundColor: 'neutralComponent',
+            }}
+            style={{ top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }}
+          >
+            <artisan.header>
+              <artisan.h1>Modal Title</artisan.h1>
+              <artisan.p>A test modal that displays placeholders instead of content</artisan.p>
+            </artisan.header>
+            <artisan.div>
+              <Button onClick={close} recipe={{ size: 'sm' }}>
+                Close
+              </Button>
+            </artisan.div>
+          </artisan.section>
+        </Portal>
+      </Show>
+    </>
     <>
       <Button onClick={open}>Open Modal</Button>
       <Show when={isOpen()}>
--- src/routes/(app)/test.tsx
+++ src/routes/(app)/test.tsx
@@ -1,13 +1,28 @@
 // for testing components
-// import { TestButton } from '@cubeartisan/cubeartisan/components/Button/Button.testrender';
-import artisan from '@cubeartisan/cubeartisan/components/factory';
-import { Modal } from '@cubeartisan/cubeartisan/components/Modal';
+import { createServerAction$, redirect } from 'solid-start/server';
+
+import { storage } from '@cubeartisan/cubeartisan/backend/user';
+import { Button } from '@cubeartisan/cubeartisan/components/Button';
+import { TestButton } from '@cubeartisan/cubeartisan/components/Button/Button.testrender';
+import { Modal } from '@cubeartisan/cubeartisan/components/Modal/Modal';
 import { VStack } from '@cubeartisan/cubeartisan/components/Stack';
 
 const Test = () => (
-  <artisan.div atoms={{ display: 'flex', flexDirection: 'column', height: 'screenH', placeItems: 'center' }}>
-    Test Component
-  </artisan.div>
+  // const [logging, signOut] = createServerAction$(async (_, { request }) => {
+  //   const session = await storage.getSession(request.headers.get('Cookie'));
+  //   return redirect('/login', {
+  //     headers: {
+  //       'Set-Cookie': await storage.destroySession(session),
+  //     },
+  //   });
+  // });
+
+  <VStack atoms={{ width: 'screenW', height: 'screenH', placeItems: 'center' }}>
+    <Modal />
+    {/* <Button onClick={() => signOut('')} recipe={{ color: 'neutral', size: 'md' }}>
+        Sign Out
+      </Button> */}
+  </VStack>
 );
 export default Test;
 
