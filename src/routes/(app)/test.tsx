2c41abf6e552d0ec85868086af2e6a53f86586a0
jesseb34r <aubin.1+github@gmail.com>
2022-12-28 23:28:55 -0800

    chore(modal): reset to basic implementation

--- src/routes/(app)/test.tsx
+++ src/routes/(app)/test.tsx
@@ -4,26 +4,24 @@ import { createServerAction$, redirect } from 'solid-start/server';
 import { storage } from '@cubeartisan/cubeartisan/backend/user';
 import { Button } from '@cubeartisan/cubeartisan/components/Button';
 import { TestButton } from '@cubeartisan/cubeartisan/components/Button/Button.testrender';
+import { Modal } from '@cubeartisan/cubeartisan/components/Modal/Modal';
 import { VStack } from '@cubeartisan/cubeartisan/components/Stack';
 
-const Test = () => {
-  const [logging, signOut] = createServerAction$(async (_, { request }) => {
-    const session = await storage.getSession(request.headers.get('Cookie'));
-    return redirect('/login', {
-      headers: {
-        'Set-Cookie': await storage.destroySession(session),
-      },
-    });
-  });
+const Test = () => (
+  // const [logging, signOut] = createServerAction$(async (_, { request }) => {
+  //   const session = await storage.getSession(request.headers.get('Cookie'));
+  //   return redirect('/login', {
+  //     headers: {
+  //       'Set-Cookie': await storage.destroySession(session),
+  //     },
+  //   });
+  // });
 
-  return (
-    <VStack atoms={{ width: 'screenW', height: 'screenH' }}>
-      <TestButton />
-      {/* <Button onClick={() => signOut('')} recipe={{ color: 'neutral', size: 'md' }}>
+  <VStack atoms={{ width: 'screenW', height: 'screenH', placeItems: 'center' }}>
+    <Modal />
+    {/* <Button onClick={() => signOut('')} recipe={{ color: 'neutral', size: 'md' }}>
         Sign Out
       </Button> */}
-    </VStack>
-  );
-};
-
+  </VStack>
+);
 export default Test;
