8246682a38df72df290fce6e18c035742eaa28d8
jesseb34r <aubin.1+github@gmail.com>
2022-12-31 21:55:35 -0800

    fix(app layout): set justify and align to 'normal'
    allow children to set formatting and don't pinch widths

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -5,8 +5,8 @@ import SiteFooter from '@cubeartisan/cubeartisan/components/templates/app/SiteFo
 import SiteNavbar from '@cubeartisan/cubeartisan/components/templates/app/SiteNavbar';
 
 const AppLayout = () => (
-  <VStack recipe={{ justify: 'normal' }}>
-    <VStack style={{ 'min-height': '100vh' }} recipe={{ justify: 'normal' }}>
+  <VStack recipe={{ justify: 'normal', align: 'normal' }}>
+    <VStack style={{ 'min-height': '100vh' }} recipe={{ justify: 'normal', align: 'normal' }}>
       <SiteNavbar />
       <main>
         <Outlet />
