91eba01d6f7656035536ab51b21250fcca9c945f
jesseb34r <aubin.1+github@gmail.com>
2023-01-01 11:33:59 -0800

    feat(test): display multiple testrender components

--- src/routes/(app)/test.tsx
+++ src/routes/(app)/test.tsx
@@ -1,11 +1,22 @@
 // for testing components
+import type { ParentComponent } from 'solid-js';
+
 import { TestButton } from '@cubeartisan/cubeartisan/components/Button/Button.testrender';
+import { Center } from '@cubeartisan/cubeartisan/components/Center';
 import { TestModal } from '@cubeartisan/cubeartisan/components/Modal/Modal.testrender';
 import { VStack } from '@cubeartisan/cubeartisan/components/Stack';
 
+const TestComponentBox: ParentComponent = (props) => (
+  <Center
+    atoms={{ minWidth: 'lg', minHeight: 'md', borderRadius: '2xl', backgroundColor: 'neutralSubtleSecondary' }}
+    children={props.children}
+  />
+);
+
 const Test = () => (
-  <VStack>
-    <TestModal />
+  <VStack style={{ 'min-height': 'calc(100vh - 4rem)' }} atoms={{ gap: 10, padding: 16 }}>
+    <TestComponentBox children={<TestModal />} />
+    <TestComponentBox children={<TestButton />} />
   </VStack>
 );
 
