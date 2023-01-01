347a91f0715edb61f43b73b236b7cae7751c03ca
jesseb34r <aubin.1+github@gmail.com>
2022-12-31 21:38:54 -0800

    chore(app layout): use VStack for styling and wrap Outlet in <main>
    Intent is for page templates within the (app) folder to return a fragment

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -1,20 +1,18 @@
 import { Outlet } from 'solid-start';
 
+import { VStack } from '@cubeartisan/cubeartisan/components/Stack';
 import SiteFooter from '@cubeartisan/cubeartisan/components/templates/app/SiteFooter';
 import SiteNavbar from '@cubeartisan/cubeartisan/components/templates/app/SiteNavbar';
-import { atoms } from '@cubeartisan/cubeartisan/styles';
 
 const AppLayout = () => (
-  <>
-    <div
-      class={atoms({
-        minHeight: 'screenH',
-      })}
-    >
+  <VStack recipe={{ justify: 'normal' }}>
+    <VStack style={{ 'min-height': '100vh' }} recipe={{ justify: 'normal' }}>
       <SiteNavbar />
-      <Outlet />
-    </div>
+      <main>
+        <Outlet />
+      </main>
+    </VStack>
     <SiteFooter />
-  </>
+  </VStack>
 );
 export default AppLayout;
