5a6889f17769336ca8b205132667cb83708aca5a
jesseb34r <aubin.1+github@gmail.com>
2023-01-07 15:44:15 -0800

    feat(SiteNavbar): Add NewCubeModal functionality

--- src/components/templates/SiteNavbar/NewCubeModal.tsx
+++ src/components/templates/SiteNavbar/NewCubeModal.tsx
@@ -2,6 +2,7 @@ import { Dialog } from '@kobalte/core';
 import { CgMathPlus } from 'solid-icons/cg';
 import { createSignal, Show } from 'solid-js';
 
+import { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button';
 import { HStack, VStack } from '@cubeartisan/cubeartisan/components/Stack';
 import { atoms } from '@cubeartisan/cubeartisan/styles';
 
@@ -10,70 +11,102 @@ const NewCubeModal = () => {
 
   return (
     <Dialog>
-      <Dialog.Trigger>
+      <Dialog.Trigger class={buttonRecipe()}>
         <CgMathPlus class={atoms({ height: 8, width: 8, color: 'white' })} />
-        Hello
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content>
-          <VStack>
-            <Dialog.Title>New Cube</Dialog.Title>
-            <VStack as="label">
-              Cube Name
-              <input type="text" />
-            </VStack>
-            <VStack as="label">
-              Visibility
+        <Dialog.Overlay
+          class={atoms({
+            backgroundColor: 'shadowDark10',
+            width: 'screenW',
+            height: 'screenH',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+          })}
+        />
+        <Dialog.Content
+          as="section"
+          class={atoms({
+            position: 'absolute',
+            paddingInline: 10,
+            paddingBlock: 8,
+            width: 'md',
+            backgroundColor: 'neutralComponent',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'stretch',
+            borderRadius: 'md',
+            gap: 4,
+          })}
+          style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+        >
+          <VStack as="header">
+            <Dialog.Title
+              class={atoms({
+                fontSize: '2xl',
+                lineHeight: '2xl',
+                fontWeight: 'semibold',
+              })}
+            >
+              New Cube
+            </Dialog.Title>
+          </VStack>
+          <VStack as="label" recipe={{ align: 'normal' }}>
+            Cube Name
+            <input type="text" />
+          </VStack>
+          <VStack as="label" recipe={{ align: 'normal' }}>
+            Visibility
+            <HStack
+              as="fieldset"
+              nam="visibility"
+              atoms={{ boxShadow: 'borderNeutralLarge', padding: 1 }}
+              recipe={{ justify: 'spaceAround' }}
+            >
+              <label>
+                <input type="radio" name="visibility" id="public" value="public" checked />
+                Public
+              </label>
+              <label>
+                <input type="radio" name="visibility" id="unlisted" value="unlisted" />
+                Unlisted
+              </label>
+              <label>
+                <input type="radio" name="visibility" id="private" value="private" />
+                Private
+              </label>
+            </HStack>
+          </VStack>
+          <VStack as="label" recipe={{ align: 'start' }}>
+            <HStack recipe={{ justify: 'spaceBetween' }} atoms={{ width: 'xs' }}>
+              Import Existing List (optional)
+              <button type="button" onClick={() => setImportSwitch((prev) => !prev)}>
+                {importSwitch() ? 'true' : 'false'}
+              </button>
+            </HStack>
+            <Show when={importSwitch()}>
               <HStack
                 as="fieldset"
-                nam="visibility"
+                name="import"
                 atoms={{ boxShadow: 'borderNeutralLarge', padding: 1, width: 'xs' }}
                 recipe={{ justify: 'spaceAround' }}
               >
                 <label>
-                  <input type="radio" name="visibility" id="public" value="public" checked />
-                  Public
+                  <input type="radio" name="import" id="paste" value="paste" />
+                  Paste Text
                 </label>
                 <label>
-                  <input type="radio" name="visibility" id="unlisted" value="unlisted" />
-                  Unlisted
+                  <input type="radio" name="import" id="file" value="file" />
+                  From File
                 </label>
                 <label>
-                  <input type="radio" name="visibility" id="private" value="private" />
-                  Private
+                  <input type="radio" name="import" id="url" value="url" />
+                  From URL
                 </label>
               </HStack>
-            </VStack>
-            <VStack as="label" recipe={{ align: 'start' }}>
-              <HStack recipe={{ justify: 'spaceBetween' }} atoms={{ width: 'xs' }}>
-                Import Existing List (optional)
-                <button type="button" onClick={() => setImportSwitch((prev) => !prev)}>
-                  {importSwitch() ? 'true' : 'false'}
-                </button>
-              </HStack>
-              <Show when={importSwitch()}>
-                <HStack
-                  as="fieldset"
-                  name="import"
-                  atoms={{ boxShadow: 'borderNeutralLarge', padding: 1, width: 'xs' }}
-                  recipe={{ justify: 'spaceAround' }}
-                >
-                  <label>
-                    <input type="radio" name="import" id="paste" value="paste" />
-                    Paste Text
-                  </label>
-                  <label>
-                    <input type="radio" name="import" id="file" value="file" />
-                    From File
-                  </label>
-                  <label>
-                    <input type="radio" name="import" id="url" value="url" />
-                    From URL
-                  </label>
-                </HStack>
-              </Show>
-            </VStack>
+            </Show>
           </VStack>
         </Dialog.Content>
       </Dialog.Portal>
