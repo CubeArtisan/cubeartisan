3a18f696a088f18a1269db1f8356fe53577db840
jesseb34r <aubin.1+github@gmail.com>
2023-01-08 23:30:57 -0800

    feat(button): use Kobalte as button base

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,6 @@
+import { Button as ButtonBase } from '@kobalte/core';
+
 import { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button/Button.css';
 import artisan from '@cubeartisan/cubeartisan/components/factory';
 
-export const Button = artisan('button', buttonRecipe);
+export const Button = artisan(typeof ButtonBase, buttonRecipe);
