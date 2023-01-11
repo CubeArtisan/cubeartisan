2e844e0166d3ba3d4040617bb77ec7f132d39b5c
jesseb34r <aubin.1+github@gmail.com>
2023-01-10 22:20:19 -0800

    chore(button): minor naming change for consistency

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
-import { Button as ButtonBase } from '@kobalte/core';
+import { Button as BaseButton } from '@kobalte/core';
 
 import { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button/Button.css';
 import artisan from '@cubeartisan/cubeartisan/components/factory';
 
-export const Button = artisan(typeof ButtonBase, buttonRecipe);
+export const Button = artisan(BaseButton, buttonRecipe);
