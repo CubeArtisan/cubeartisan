a151e4e0a1491a165f614207ab5f9b7937079402
jesseb34r <aubin.1+github@gmail.com>
2023-01-14 14:29:09 -0800

    chore(button): remove unused imports

--- src/components/Button/Button.testrender.css.ts
+++ src/components/Button/Button.testrender.css.ts
@@ -1,7 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-import { vars } from '@cubeartisan/cubeartisan/styles';
-
 export const testButtonContainer = style({
   display: 'grid',
   placeItems: 'center',
