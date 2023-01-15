b90b4f65acaadd88869b585ca0e0456ae1802960
jesseb34r <aubin.1+github@gmail.com>
2023-01-14 20:36:30 -0800

    feat(button): implement recipe's for every color

--- src/components/Button/Button.css.ts
+++ src/components/Button/Button.css.ts
@@ -25,13 +25,132 @@ export const buttonRecipe = recipe({
         background: vars.color.neutral3,
         outline: `solid ${vars.borderSize.standard} ${vars.color.neutral6}`,
 
-        ':hover': {
-          background: vars.color.neutral4,
-          outline: `solid ${vars.borderSize.standard} ${vars.color.neutral7}`,
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.neutral4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.neutral5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
         },
+      },
+      primary: {
+        background: vars.color.primary3,
+        outline: `solid ${vars.borderSize.standard} ${vars.color.primary6}`,
 
-        ':active': {
-          background: vars.color.neutral5,
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.primary4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.primary7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.primary5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
+        },
+      },
+      success: {
+        background: vars.color.success3,
+        outline: `solid ${vars.borderSize.standard} ${vars.color.success6}`,
+
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.success4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.success7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.success5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
+        },
+      },
+      info: {
+        background: vars.color.info3,
+        outline: `solid ${vars.borderSize.standard} ${vars.color.info6}`,
+
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.info4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.info7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.info5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
+        },
+      },
+      warning: {
+        background: vars.color.warning3,
+        outline: `solid ${vars.borderSize.standard} ${vars.color.warning6}`,
+
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.warning4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.warning7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.warning5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
+        },
+      },
+      danger: {
+        background: vars.color.danger3,
+        outline: `solid ${vars.borderSize.standard} ${vars.color.danger6}`,
+
+        selectors: {
+          '&[data-hover]': {
+            background: vars.color.danger4,
+            outline: `solid ${vars.borderSize.standard} ${vars.color.danger7}`,
+          },
+          '&[data-active]': {
+            background: vars.color.danger5,
+          },
+          '&[focus-active]': {
+            outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.primary7}`,
+          },
+          '&[data-disabled]': {
+            outline: `solid ${vars.borderSize.standard} ${vars.color.neutral5}`,
+            background: vars.color.neutral2,
+            cursor: 'not-allowed',
+          },
         },
       },
     },
