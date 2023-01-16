a226361de873aa7bd22a85453502d726f74f082a
jesseb34r <aubin.1+github@gmail.com>
2023-01-15 18:35:46 -0800

    fix(Button): handle class prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,13 @@
-import { Button as BaseButton, ButtonOptions as BaseButtonOptions } from '@kobalte/core';
-import { ParentComponent, splitProps } from 'solid-js';
+import { Button as BaseButton } from '@kobalte/core';
+import clsx from 'clsx';
+import { ComponentProps, ParentComponent, splitProps } from 'solid-js';
 
 import { buttonRecipe, ButtonVariants } from '@cubeartisan/cubeartisan/components/Button/Button.css';
 
-export const ButtonRoot: ParentComponent<{ recipe?: ButtonVariants } & BaseButtonOptions> = (props) => {
-  const [local, others] = splitProps(props, ['recipe']);
+export const ButtonRoot: ParentComponent<{ recipe?: ButtonVariants } & ComponentProps<typeof BaseButton.Root>> = (
+  props,
+) => {
+  const [local, others] = splitProps(props, ['recipe', 'class']);
 
-  return <BaseButton.Root class={buttonRecipe(local.recipe)} {...others} />;
+  return <BaseButton.Root class={clsx(buttonRecipe(local.recipe), local.class)} {...others} />;
 };
