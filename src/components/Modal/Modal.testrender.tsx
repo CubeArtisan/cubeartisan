e64aaca926387d806793a84cf5c3fe3a81795358
jesseb34r <aubin.1+github@gmail.com>
2023-01-15 19:12:56 -0800

    feat(Modal): use buttonRecipe for styling

--- src/components/Modal/Modal.testrender.tsx
+++ src/components/Modal/Modal.testrender.tsx
@@ -1,9 +1,10 @@
+import { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button';
 import { Modal } from '@cubeartisan/cubeartisan/components/Modal';
 import * as styles from '@cubeartisan/cubeartisan/components/Modal/Modal.testrender.css';
 
 export const TestModal = () => (
   <Modal.Root>
-    <Modal.Trigger>Open Modal</Modal.Trigger>
+    <Modal.Trigger class={buttonRecipe({ padding: 'baseText' })}>Open Modal</Modal.Trigger>
     <Modal.Portal>
       <Modal.Overlay />
       <Modal.Content>
@@ -17,7 +18,7 @@ export const TestModal = () => (
           aute dolore. Ea nostrud esse duis esse exercitation proident anim ipsum laborum. Ad duis veniam enim laboris
           laboris qui incididunt eu ipsum dolor occaecat amet exercitation dolore.
         </p>
-        <Modal.CloseButton>Close Modal</Modal.CloseButton>
+        <Modal.CloseButton class={buttonRecipe({ padding: 'baseText' })}>Close Modal</Modal.CloseButton>
       </Modal.Content>
     </Modal.Portal>
   </Modal.Root>
