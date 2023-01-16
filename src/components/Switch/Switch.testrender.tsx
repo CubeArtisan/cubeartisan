e43f059551624656103b9b2ae439307f045d782b
jesseb34r <aubin.1+github@gmail.com>
2023-01-15 20:12:20 -0800

    style(Switch.testrender): make checked label prettier

--- src/components/Switch/Switch.testrender.tsx
+++ src/components/Switch/Switch.testrender.tsx
@@ -7,7 +7,7 @@ export const TestSwitch = () => {
   const [checked, setChecked] = createSignal(false);
 
   return (
-    <>
+    <div class={styles.testContainer}>
       <Switch.Root isChecked={checked()} onCheckedChange={setChecked}>
         <Switch.Input />
         <Switch.Label>Test Switch</Switch.Label>
@@ -15,7 +15,15 @@ export const TestSwitch = () => {
           <Switch.Thumb />
         </Switch.Control>
       </Switch.Root>
-      <span class={styles.toggleLabel}>{checked() ? 'on' : 'off'}</span>
-    </>
+      <span
+        classList={{
+          [styles.toggleLabel]: true,
+          [styles.toggleChecked]: checked(),
+          [styles.toggleUnchecked]: !checked(),
+        }}
+      >
+        {checked() ? 'on' : 'off'}
+      </span>
+    </div>
   );
 };
