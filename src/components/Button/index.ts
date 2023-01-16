d278bf1af61834c0999c30262d0a3a0182fab340
jesseb34r <aubin.1+github@gmail.com>
2023-01-15 19:12:12 -0800

    feat(Button): export buttonRecipe

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,3 +1,4 @@
 import { ButtonRoot as Root } from '@cubeartisan/cubeartisan/components/Button/Button';
 
 export const Button = { Root };
+export { buttonRecipe } from '@cubeartisan/cubeartisan/components/Button/Button.css';
