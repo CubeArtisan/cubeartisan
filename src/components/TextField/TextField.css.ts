78c5909eb56a2ae74d9b7e891c7ab82ebeba452d
jesseb34r <aubin.1+github@gmail.com>
2023-01-17 22:32:02 -0800

    feat(components): add TextField component

---
+++ src/components/TextField/TextField.css.ts
@@ -0,0 +1,51 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@cubeartisan/cubeartisan/styles';
+
+export const textfieldRoot = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: vars.space['2.5'],
+  width: vars.size.md,
+});
+
+export const textfieldInput = style({
+  display: 'inline-flex',
+  borderRadius: vars.borderRadius.md,
+  paddingInline: vars.space[2],
+  paddingBlock: vars.space[1],
+  backgroundColor: vars.color.neutral3,
+  outline: `solid ${vars.borderSize.standard} ${vars.color.neutral6}`,
+
+  selectors: {
+    '&[data-hover]': {
+      backgroundColor: vars.color.neutral4,
+      outline: `solid ${vars.borderSize.standard} ${vars.color.neutral7}`,
+    },
+    '&[data-focus-visible]': {
+      outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.neutral7}`,
+    },
+    '&[data-invalid]': {
+      outline: `solid ${vars.borderSize.standard} ${vars.color.danger7}`,
+    },
+    '&::placeholder': {
+      color: vars.color.neutral11,
+    },
+  },
+});
+
+export const textfieldDescription = style({
+  fontSize: vars.fontSize.sm,
+  color: vars.color.neutral11,
+
+  selectors: {
+    '&[data-invalid]': {
+      display: 'none',
+    },
+  },
+});
+
+export const textfieldErrorMessage = style({
+  fontSize: vars.fontSize.sm,
+  color: vars.color.danger9,
+});
--- src/routes/(app)/test.tsx
+++ src/routes/(app)/test.tsx
@@ -4,21 +4,25 @@ import { TestModal } from '@cubeartisan/cubeartisan/components/Modal/Modal.testr
 import { TestRadioGroup } from '@cubeartisan/cubeartisan/components/RadioGroup/RadioGroup.testrender';
 import { TestSwitch } from '@cubeartisan/cubeartisan/components/Switch/Switch.testrender';
 import ContentPage from '@cubeartisan/cubeartisan/components/templates/ContentPage/ContentPage';
+import { TestTextField } from '@cubeartisan/cubeartisan/components/TextField/TextField.testrender';
 import * as styles from '@cubeartisan/cubeartisan/routes/(app)/test.css';
 
 const Test = () => (
   <ContentPage class={styles.testPage}>
     <div class={styles.testComponentBox}>
-      <TestModal />{' '}
-    </div>
-    <div class={styles.testComponentBox}>
-      <TestRadioGroup />
+      <TestButton />
     </div>
     <div class={styles.testComponentBox}>
       <TestSwitch />
     </div>
     <div class={styles.testComponentBox}>
-      <TestButton />
+      <TestTextField />
+    </div>
+    <div class={styles.testComponentBox}>
+      <TestRadioGroup />
+    </div>
+    <div class={styles.testComponentBox}>
+      <TestModal />
     </div>
   </ContentPage>
 );
