a96f98ee69f04b5b7d628ffb16a497ea0310f5d0
jesseb34r <jessea.login@gmail.com>
2023-02-23 15:07:15 -0800

    style(button): add more interactive button states

--- src/components/generic/Button/Button.css.ts
+++ src/components/generic/Button/Button.css.ts
@@ -4,11 +4,14 @@ import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 import { vars } from '@cubeartisan/cubeartisan/styles';
 
 const buttonColor = createThemeContract({
+  baseTextColor: null,
+  hoverTextColor: null,
   baseBgColor: null,
-  baseOutlineColor: null,
   hoverBgColor: null,
-  hoverOutlineColor: null,
   activeBgColor: null,
+  baseOutlineColor: null,
+  hoverOutlineColor: null,
+  activeOutlineColor: null,
 });
 
 export const buttonRecipe = recipe({
@@ -18,21 +21,24 @@ export const buttonRecipe = recipe({
     alignItems: 'center',
     borderRadius: vars.borderRadius.md,
 
-    background: buttonColor.baseBgColor,
+    color: buttonColor.baseTextColor,
+    backgroundColor: buttonColor.baseBgColor,
     outline: `solid ${vars.borderSize.standard} ${buttonColor.baseOutlineColor}`,
 
     ':hover': {
-      background: buttonColor.hoverBgColor,
+      color: buttonColor.hoverTextColor,
+      backgroundColor: buttonColor.hoverBgColor,
       outlineColor: buttonColor.hoverOutlineColor,
     },
     ':active': {
-      background: buttonColor.activeBgColor,
+      backgroundColor: buttonColor.activeBgColor,
+      outlineColor: buttonColor.activeOutlineColor,
     },
     ':focus': {
       outline: `solid ${vars.borderSize['focus-ring']} ${vars.color.neutral7}`,
     },
     ':disabled': {
-      background: vars.color.neutral2,
+      backgroundColor: vars.color.neutral2,
       outlineColor: vars.color.neutral5,
       cursor: 'not-allowed',
     },
@@ -41,56 +47,74 @@ export const buttonRecipe = recipe({
     color: {
       neutral: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.neutral11,
+          hoverTextColor: vars.color.neutral12,
           baseBgColor: vars.color.neutral3,
-          baseOutlineColor: vars.color.neutral6,
           hoverBgColor: vars.color.neutral4,
+          activeBgColor: vars.color.neutral5,
+          baseOutlineColor: vars.color.neutral6,
           hoverOutlineColor: vars.color.neutral7,
-          activeBgColor: vars.color.neutral8,
+          activeOultineColor: vars.color.neutral8,
         }),
       },
       primary: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.primary11,
+          hoverTextColor: vars.color.primary12,
           baseBgColor: vars.color.primary3,
-          baseOutlineColor: vars.color.primary6,
           hoverBgColor: vars.color.primary4,
+          activeBgColor: vars.color.primary5,
+          baseOutlineColor: vars.color.primary6,
           hoverOutlineColor: vars.color.primary7,
-          activeBgColor: vars.color.primary8,
+          activeOultineColor: vars.color.primary8,
         }),
       },
       success: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.success11,
+          hoverTextColor: vars.color.success12,
           baseBgColor: vars.color.success3,
-          baseOutlineColor: vars.color.success6,
           hoverBgColor: vars.color.success4,
+          activeBgColor: vars.color.success5,
+          baseOutlineColor: vars.color.success6,
           hoverOutlineColor: vars.color.success7,
-          activeBgColor: vars.color.success8,
+          activeOultineColor: vars.color.success8,
         }),
       },
       info: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.info11,
+          hoverTextColor: vars.color.info12,
           baseBgColor: vars.color.info3,
-          baseOutlineColor: vars.color.info6,
           hoverBgColor: vars.color.info4,
+          activeBgColor: vars.color.info5,
+          baseOutlineColor: vars.color.info6,
           hoverOutlineColor: vars.color.info7,
-          activeBgColor: vars.color.info8,
+          activeOultineColor: vars.color.info8,
         }),
       },
       warning: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.warning11,
+          hoverTextColor: vars.color.warning12,
           baseBgColor: vars.color.warning3,
-          baseOutlineColor: vars.color.warning6,
           hoverBgColor: vars.color.warning4,
+          activeBgColor: vars.color.warning5,
+          baseOutlineColor: vars.color.warning6,
           hoverOutlineColor: vars.color.warning7,
-          activeBgColor: vars.color.warning8,
+          activeOultineColor: vars.color.warning8,
         }),
       },
       danger: {
         vars: assignVars(buttonColor, {
+          baseTextColor: vars.color.danger11,
+          hoverTextColor: vars.color.danger12,
           baseBgColor: vars.color.danger3,
-          baseOutlineColor: vars.color.danger6,
           hoverBgColor: vars.color.danger4,
+          activeBgColor: vars.color.danger5,
+          baseOutlineColor: vars.color.danger6,
           hoverOutlineColor: vars.color.danger7,
-          activeBgColor: vars.color.danger8,
+          activeOultineColor: vars.color.danger8,
         }),
       },
     },
