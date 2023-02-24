3ce3051134f2c4af41b080c538e75a7bb5777be2
jesseb34r <jessea.login@gmail.com>
2023-02-23 22:14:37 -0800

    feat(cube hero): add actions, update styles, and use mock data

--- src/components/cube/list/ListHero/ListHero.css.ts
+++ src/components/cube/list/ListHero/ListHero.css.ts
@@ -2,10 +2,6 @@ import { style } from '@vanilla-extract/css';
 
 import { vars } from '@cubeartisan/cubeartisan/styles';
 
-export const heroContainer = style({
-  marginBottom: vars.space[5],
-});
-
 export const heroRoot = style({
   minHeight: vars.space[40],
   width: vars.size['content-80'],
@@ -14,20 +10,62 @@ export const heroRoot = style({
   alignItems: 'start',
   justifyItems: 'start',
   gridTemplateColumns: '1fr 1fr',
-  paddingInline: vars.space[5],
-  paddingBlock: vars.space[8],
+  paddingInline: vars.space.gutter,
+  marginTop: vars.space.gutter,
 });
 
-export const cubeName = style({
-  fontSize: vars.fontSize['3xl'],
-  lineHeight: vars.lineHeight['3xl'],
+export const heroTitleSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export const cubeTitle = style({
+  fontSize: vars.fontSize['4xl'],
+  lineHeight: vars.lineHeight['4xl'],
   fontWeight: vars.fontWeight.bold,
-  borderRadius: vars.borderRadius.sm,
+  marginBottom: vars.space['2.5'],
 });
 
-export const cubeOwnerName = style({
+export const cubeSubtitle = style({
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: vars.fontSize.xl,
+  lineHeight: vars.lineHeight.xl,
   fontWeight: vars.fontWeight.semibold,
   color: vars.color.neutral11,
+  marginBottom: vars.space.gutter,
+});
+
+export const cubeOwner = style({
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: vars.space['1.5'],
+  marginLeft: vars.space['2.5'],
+});
+
+export const cubeOwnerAvatar = style({
+  width: vars.space[6],
+  height: vars.space[6],
+});
+
+export const heroActions = style({
+  display: 'flex',
+  gap: vars.space[8],
+  alignItems: 'center',
+});
+
+export const heroActionButton = style({
+  color: vars.color.neutral11,
+
+  ':hover': {
+    color: vars.color.neutral12,
+  },
+});
+
+export const heroActionIcon = style({
+  height: vars.space[6],
+  width: vars.space[6],
 });
 
 export const cubeDescriptionHeader = style({
@@ -37,11 +75,13 @@ export const cubeDescriptionHeader = style({
 
 export const cubeDescription = style({
   maxWidth: '40ch',
+  color: vars.color.neutral11,
 });
 
-export const separator = style({
-  marginInline: 'auto',
+export const heroSeparator = style({
   width: vars.size['content-90'],
   height: '1px',
+  marginInline: 'auto',
+  marginBottom: vars.space[5],
   backgroundColor: vars.color.neutral12,
 });
