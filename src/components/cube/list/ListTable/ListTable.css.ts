7b89eba160af24575ce74043271d74f729748291
jesseb34r <jessea.login@gmail.com>
2023-02-24 13:11:07 -0800

    feat(list view): use sorted mock data and display sorted sections

--- src/components/cube/list/ListTable/ListTable.css.ts
+++ src/components/cube/list/ListTable/ListTable.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { createVar, style } from '@vanilla-extract/css';
 
 import { vars } from '@cubeartisan/cubeartisan/styles';
 
@@ -17,35 +17,79 @@ export const tableContainer = style({
 export const table = style({
   display: 'flex',
   gap: vars.space['2.5'],
-  alignItems: 'start',
   marginInline: 'auto',
-  padding: vars.space['2.5'],
 });
 
 export const tableColumn = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'stretch',
+  maxWidth: vars.space[40],
 });
 
 export const tableColumnTitle = style({
-  alignSelf: 'center',
+  textAlign: 'center',
   fontSize: vars.fontSize.lg,
   fontWeight: vars.fontWeight.semibold,
   marginBottom: vars.space['2.5'],
 });
 
 export const tableColumnContent = style({
-  paddingBlock: vars.space[1],
-  paddingInline: vars.space['2.5'],
   borderRadius: vars.borderRadius.md,
   color: vars.color.neutral11,
-  outline: `solid ${vars.borderSize.standard} ${vars.color.neutral11}`,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: vars.space['2.5'],
 });
 
-export const tableColumnItem = style({
-  fontSize: vars.fontSize.base,
+export const tableColumnSection = style({
+  outline: `solid ${vars.borderSize.large} ${vars.color.neutral6}`,
+  borderRadius: vars.borderRadius.md,
   overflow: 'hidden',
+});
+
+export const tableColumnSectionTitle = style({
+  color: vars.color.neutral12,
+  textAlign: 'center',
+  textTransform: 'capitalize',
+  fontSize: vars.fontSize.sm,
+  lineHeight: vars.fontSize.sm,
+  paddingBlock: vars.space[1],
+  background: vars.color.neutral3,
+});
+
+export const tableColumnSectionDivider = style({
+  height: '1px',
+  backgroundColor: vars.color.neutral9,
+  width: '100%',
+});
+
+export const cardBackground = createVar();
+export const tableCardItemButton = style({
+  position: 'relative',
+  backgroundColor: cardBackground,
+
+  paddingInline: vars.space['1.5'],
+  paddingBlock: vars.space['0.5'],
+  width: '100%',
+  textAlign: 'left',
+
+  ':hover': {
+    color: vars.color.neutral12,
+  },
+
+  selectors: {
+    '&:hover:after': {
+      content: '',
+      position: 'absolute',
+      inset: 0,
+      backgroundColor: vars.color.shadow4,
+    },
+  },
+});
+
+export const tableCardItemText = style({
+  display: 'block',
+  fontSize: vars.fontSize.sm,
+  lineHeight: vars.lineHeight.sm,
   textOverflow: 'ellipsis',
   whiteSpace: 'nowrap',
+  overflow: 'hidden',
 });
--- src/components/cube/list/ListTable/ListTable.tsx
+++ src/components/cube/list/ListTable/ListTable.tsx
@@ -1,59 +1,101 @@
-import { Component, For } from 'solid-js';
+import { Button, Separator } from '@kobalte/core';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import { Component, For, Show } from 'solid-js';
 
 import * as styles from '@cubeartisan/cubeartisan/components/cube/list/ListTable/ListTable.css';
+import { testCubeSorted } from '@cubeartisan/cubeartisan/mock/testCubeSorted';
 
-const TableColumn: Component<{ title: string; content: string[]; backgroundColor: string }> = (props) => (
-  <section class={styles.tableColumn}>
-    <h2 class={styles.tableColumnTitle}>{props.title}</h2>
-    <ul class={styles.tableColumnContent} style={{ 'background-color': props.backgroundColor }}>
-      <For each={props.content}>
-        {(item) => (
-          <li>
-            <p class={styles.tableColumnItem}>{item}</p>
-          </li>
-        )}
-      </For>
-    </ul>
-  </section>
-);
-
+/**
+ * # Planning
+ * Columns by color
+ * Section by type
+ * Divide within section by cost
+ * Sort within cost by name
+ */
 export const ListTable = () => {
-  const mockCards = [
-    'lightning bolt',
-    'mother of runes',
-    'shock',
-    'stoneforge mystic',
-    'kaldra compleat',
-    "umezawa's jitte",
-    'lightning bolt',
-    'mother of runes',
-    'shock',
-    'stoneforge mystic',
-    'kaldra compleat',
-    "umezawa's jitte",
-    'lightning bolt',
-    'mother of runes',
-    'shock',
-    'stoneforge mystic',
-    'kaldra compleat',
-    "umezawa's jitte",
-    'lightning bolt',
-    'mother of runes',
-    'shock',
-  ];
+  const cube = testCubeSorted;
+
+  type Card = {
+    name: string;
+    color: 'W' | 'U' | 'B' | 'R' | 'G' | 'M' | 'C' | 'L';
+    cmc: number;
+    type: 'creature' | 'planeswalker' | 'instant' | 'sorcery' | 'enchantment' | 'artifact' | 'land';
+  };
+
+  const colorCodeMap = {
+    W: 'hsl(50, 100%, 80%, 0.2)',
+    U: 'hsl(220, 100%, 65%, 0.2)',
+    B: 'hsl(260, 40%, 50%, 0.2)',
+    R: 'hsl(0, 100%, 65%, 0.2)',
+    G: 'hsl(100, 100%, 65%, 0.2)',
+    M: 'hsl(50, 100%, 40%, 0.2)',
+    C: 'hsl(0, 0%, 40%, 0.2)',
+    L: 'hsl(20, 100%, 40%, 0.2)',
+  };
+
+  const colorNameMap = {
+    W: 'White',
+    U: 'Blue',
+    B: 'Black',
+    R: 'Red',
+    G: 'Green',
+    M: 'Multicolor',
+    C: 'Colorless',
+    L: 'Lands',
+  };
+
+  const CardItem: Component<{ card: Card }> = (props) => (
+    <Button.Root
+      as={'li'}
+      class={styles.tableCardItemButton}
+      style={assignInlineVars({ [styles.cardBackground]: colorCodeMap[props.card.color] })}
+    >
+      <span class={styles.tableCardItemText}>{props.card.name}</span>
+    </Button.Root>
+  );
+
+  const CardCostSection: Component<{ cards: Card[] }> = (props) => (
+    <For each={props.cards}>{(card) => <CardItem card={card} />}</For>
+  );
+
+  const CardTypeSection: Component<{ cards: Card[][] }> = (props) => {
+    const numCardsInSection = () => {
+      let total = 0;
+      props.cards.forEach((subArray) => {
+        total += subArray.length;
+      });
+      return total;
+    };
+
+    return (
+      <ul class={styles.tableColumnSection}>
+        <h3 class={styles.tableColumnSectionTitle}>{`${props.cards[0][0].type} (${numCardsInSection()})`}</h3>
+        <For each={props.cards}>
+          {(cardCostSection) => (
+            <>
+              <CardCostSection cards={cardCostSection} />
+              <Show when={numCardsInSection() >= 5 && cardCostSection !== props.cards[props.cards.length - 1]}>
+                <Separator.Root class={styles.tableColumnSectionDivider} />
+              </Show>
+            </>
+          )}
+        </For>
+      </ul>
+    );
+  };
+
+  const CardColorColumn: Component<{ cards: Card[][][] }> = (props) => (
+    <div class={styles.tableColumn}>
+      <h2 class={styles.tableColumnTitle}>{colorNameMap[props.cards[0][0][0]?.color]}</h2>
+      <div class={styles.tableColumnContent}>
+        <For each={props.cards}>{(cardTypeSection) => <CardTypeSection cards={cardTypeSection} />}</For>
+      </div>
+    </div>
+  );
 
   return (
-    <div class={styles.tableContainer}>
-      <div class={styles.table}>
-        <TableColumn title="White" content={mockCards} backgroundColor="hsl(50, 100%, 80%, 0.2)" />
-        <TableColumn title="Blue" content={mockCards} backgroundColor="hsl(220, 100%, 65%, 0.2)" />
-        <TableColumn title="Black" content={mockCards} backgroundColor="hsl(260, 40%, 50%, 0.2)" />
-        <TableColumn title="Red" content={mockCards} backgroundColor="hsl(0, 100%, 65%, 0.2)" />
-        <TableColumn title="Green" content={mockCards} backgroundColor="hsl(100, 100%, 65%, 0.2)" />
-        <TableColumn title="Multicolor" content={mockCards} backgroundColor="hsl(50, 100%, 40%, 0.2)" />
-        <TableColumn title="Colorless" content={mockCards} backgroundColor="hsl(0, 0%, 40%, 0.2)" />
-        <TableColumn title="Lands" content={mockCards} backgroundColor="hsl(20, 100%, 40%, 0.2)" />
-      </div>
+    <div class={styles.table}>
+      <For each={cube.boards[0]?.cards}>{(cardColorSection) => <CardColorColumn cards={cardColorSection} />}</For>
     </div>
   );
 };
