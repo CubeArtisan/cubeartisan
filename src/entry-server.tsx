2ec11a0523f06afeb9cfbd1a585584144441ae65
Devon Richards <friends.devon@gmail.com>
2023-02-24 19:29:43 -0600

    feat(carddb): Add the ability to load the carddb to the client.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,7 +1,10 @@
 import { createHandler, renderAsync, StartServer } from 'solid-start/entry-server';
 
 import getMongoConnection from '@cubeartisan/cubeartisan/backend/mongoConnection';
+import { loadCardDb } from '@cubeartisan/cubeartisan/backend/carddb';
 
 getMongoConnection();
+
+loadCardDb();
 
 export default createHandler(renderAsync((event) => <StartServer event={event} />));
